4d68cc80d3d3b1be38d411fd7dc686e6ea753c81
samedalp <Thinkdifferent53>
2020-06-17 22:34:43 +0300

    CODE-03-SAM

--- public/back/js/demo/chart-area-demo.js
+++ public/back/js/demo/chart-area-demo.js
@@ -49,6 +49,7 @@ var myLineChart = new Chart(ctx, {
       data: [0, 20000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
     }],
   },
+
   options: {
     maintainAspectRatio: false,
     layout: {
