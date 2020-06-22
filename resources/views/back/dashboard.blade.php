79b125172f5325cd8d0d72344020d15cbea55eb9
AYOSMEDYA <ayosmedya@gmail.com>
2020-06-22 22:31:21 +0300

    CODE-AMO-7

--- resources/views/back/dashboard.blade.php
+++ resources/views/back/dashboard.blade.php
@@ -154,6 +154,7 @@ $youtube= $demandsGraph['youtube']*$oran;
 
             <div class="col-lg-6 mb-4">
 
+
               <!-- Illustrations -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
